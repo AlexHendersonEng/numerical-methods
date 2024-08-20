aa05f479df58c1565874be4fde2d6dd26bc8d8d8
AlexHendersonEng <Alexhendersnao@gmail.com>
2024-08-20 22:31:47 +0100

    Added gradient descent optimiser function

---
+++ MATLAB/optimisers/gradient_descent.m
@@ -0,0 +1,66 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% gradient_descent function for implementation of the gradient descent
+% optimisation algorithm
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+function [x, f_val] = gradient_descent(f, x0, options)
+%
+%   Input validation
+%
+    arguments
+        f
+        x0
+        options.max_iter = 100
+        options.h = 1e-3
+        options.lb = repmat(-100, size(x0))
+        options.ub = repmat(100, size(x0))
+        options.Display = true
+    end
+%
+%   Initial setup
+%
+    x = x0;
+    fx = f(x);
+    f_val = fx;
+%
+%   Solve loop
+%
+    for iter = 1 : options.max_iter
+%
+%       Approximate the derivative using finite difference
+%
+        dfx = zeros(size(x));
+        for x_i = 1 : numel(x)
+            dx = zeros(size(x));
+            dx(x_i) = 1e-6;
+            dfx(x_i) = (f(x + dx) - fx) / 1e-6;
+        end
+%
+%       Calculate the next guess using the Newton-Raphson formula
+%
+        x = x - options.h * dfx;
+%
+%       Apply bounds
+%
+        x = min(max(x, options.lb), options.ub);
+%
+%       Calculate the function value at the current guess
+%
+        fx = f(x);
+        if fx < f_val
+            f_val = fx;
+        end
+%
+%       Command window output
+%
+        if options.Display
+            disp("Iter: " + num2str(iter) + ", f_val: " + num2str(f_val));
+        end
+    end
+end
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% End
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
