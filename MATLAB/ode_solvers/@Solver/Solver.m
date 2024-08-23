a0d74fe5a2a9405a514ef1d46f2fba682c214134
AlexHendersonEng <Alexhendersnao@gmail.com>
2024-08-23 21:53:19 +0100

    Added ODE solver superclass

---
+++ MATLAB/ode_solvers/@Solver/Solver.m
@@ -0,0 +1,32 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Solver superclass for ODE solvers
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+classdef Solver < handle
+%
+    properties
+        h
+        t
+    end
+%
+    methods
+        function obj = Solver(t0, h)
+%
+%           Initialise solver time and step size
+%
+            obj.t = t0;
+            obj.h = h;
+        end
+%
+        update(obj);
+%
+        step(obj, ode_fun, y0);
+    end
+%
+end
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% End
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
