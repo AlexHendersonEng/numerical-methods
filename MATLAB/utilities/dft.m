4b84efc350873ba952bc1f40d9a850649ea423f0
AlexHendersonEng <Alexhendersnao@gmail.com>
2024-09-13 14:12:13 +0100

    Minor formatting fix in discrete Fourier transform function

--- MATLAB/utilities/dft.m
+++ MATLAB/utilities/dft.m
@@ -12,6 +12,7 @@ function X = dft(x)
     X = zeros(size(x));
 %
 % Compute discrete Fourier transform
+%
     for k = 1 : N
         for n = 1 : N
             X(k) = X(k) + x(n) * exp(-2j * pi * (k - 1) * (n - 1) / N);
